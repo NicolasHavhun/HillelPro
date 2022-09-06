23a520cc49fbdee6e583976f9f9f8f5c2b1bd32d
Nicolas Havhun <nicolas.havhun@gmail.com>
2022-09-06 16:30:55 +0300

    2

--- src/main/java/com/hillelPro/havhun/homework2/House.java
+++ src/main/java/com/hillelPro/havhun/homework2/House.java
@@ -1,4 +1,23 @@
 package com.hillelPro.havhun.homework2;
 
 public class House {
+    private class TheTable {
+        int length = 120;
+        int width = 80;
+        int height = 90;
+    }
+
+    private class TheStairs{
+        int stepWidth = 65;
+        int quantitySteps = 11;
+        String colorStairs = "dark walnut";
+    }
+
+    private class AirConditioner{
+        String title = "Gree";
+        double power = 2.5;
+        boolean inverterModification =true;
+
+    }
+
 }
